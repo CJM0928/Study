01eae6aa182ca54470b8f61c25b15142158a7cf5
powerkimchi <135217812+powerkimchi@users.noreply.github.com>
2023-11-23 21:14:05 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* my_string, const char* is_prefix) {
+    int my_l = strlen(my_string);
+    int is_l = strlen(is_prefix);
+    //문자열의 길이 비교
+    if(my_l < is_l){
+        return 0;
+    }
+    //접두사 비교
+    for(int i = 0; i < is_l; i++ ){
+        if(my_string[i] != is_prefix[i]){
+            return 0;
+        }
+    }
+    // 둘다 문제 없다면 1을 출력
+    return 1;
+}
