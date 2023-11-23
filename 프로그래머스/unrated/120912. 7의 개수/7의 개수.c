1f36ee895854a2bf54abea7433c63111b5a14347
powerkimchi <135217812+powerkimchi@users.noreply.github.com>
2023-11-23 23:12:16 +0900

    [level unrated] Title: 7의 개수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// array_len은 배열 array의 길이입니다.
+int solution(int array[], size_t array_len) {
+    int answer = 0;
+    int l = array_len;
+    int s = 0;
+    for(int i = 0; i < l; i++){
+        while(array[i] != 0){
+            if(array[i] % 10 == 7){
+                answer++;
+            }
+            array[i] /= 10;
+        }
+    }
+    return answer;
+}
