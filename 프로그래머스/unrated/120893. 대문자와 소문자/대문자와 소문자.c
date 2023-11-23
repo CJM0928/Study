91a7db2b8c0f3e7e07175e07e530c6e774a70f9e
powerkimchi <135217812+powerkimchi@users.noreply.github.com>
2023-11-23 23:03:24 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* my_string) {
+    int l = strlen(my_string);
+    int idx = 0;
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc((l+1)*sizeof(char));
+    for(int i = 0 ; i < l; i ++){
+        if('A' <= my_string[i] && my_string[i] <= 'Z'){
+            answer[idx++] = my_string[i] + 32;
+        }else if('a' <= my_string[i] && my_string[i] <= 'z'){
+            answer[idx++] = my_string[i] - 32;
+        }
+    }
+    answer[idx] = '\0';
+    return answer;
+}
