e096632d7ba7bc65616ba910f61cc63c7fce1b65
powerkimchi <135217812+powerkimchi@users.noreply.github.com>
2023-11-23 22:07:24 +0900

    [level unrated] Title: 배열 만들기 6, Time: 0.62 ms, Memory: 6.36 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// arr_len은 배열 arr의 길이입니다.
+int* solution(int arr[], size_t arr_len) {
+    int l = arr_len;
+    int idx = 0;
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc((l+1)*sizeof(int));
+    for(int i = 0; i<l; i++){
+        if(idx == 0){
+            answer[idx++] = arr[i];
+        }else if(answer[idx - 1] == arr[i]){
+            answer[--idx] = '\0';
+        }else if(answer[idx - 1] != arr[i]){
+            answer[idx++] = arr[i];
+        }
+    }
+    if(idx == 0){
+        answer[idx] = -1;
+    }
+    
+    return answer;
+}
