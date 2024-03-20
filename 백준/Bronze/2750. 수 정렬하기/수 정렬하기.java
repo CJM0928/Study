e4d74699092f13d54938811e2e86074402c59b64
CJM0928 <135217812+CJM0928@users.noreply.github.com>
2024-03-20 16:03:31 +0900

    [Bronze II] Title: 수 정렬하기, Time: 184 ms, Memory: 16480 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,41 +1,44 @@
-import java.util.Scanner;
-
-// 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다.
-// 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
-// N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+import java.io.*;
 
 public class Main {
 
-    Scanner kb;
+    BufferedReader br;
 
-    int [] number;
-    private void sort(){
-        kb = new Scanner(System.in);
-        int num = kb.nextInt();
-        number = new int[num];
+    BufferedWriter bw;
 
-        for(int i=0; i<num; i++){
-            number[i] = kb.nextInt();
+    int N;
+
+    int [] num;
+
+    private void Bubblesort() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        num = new int[N];
+
+        for(int i=0; i<N; i++){
+            num[i] = Integer.parseInt(br.readLine());
         }
 
-        for(int i=num-1; i>0; i--){
+        for(int i=N-1; i>0; i--){
             for(int j=0; j<i; j++){
-                if(number[j] > number[j+1]){
-                    int temp = number[j];
-                    number[j] = number[j+1];
-                    number[j+1] = temp;
+                if(num[j] > num[j+1]){
+                    int temp = num[j];
+                    num[j] = num[j+1];
+                    num[j+1] = temp;
                 }
             }
         }
 
-        for(int i=0; i<num; i++){
-            System.out.println(number[i]);
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(int i=0; i<N; i++){
+            bw.write(num[i]+"\n");
         }
-        kb.close();
+        bw.flush();
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException{
         Main App = new Main();
 
-        App.sort();
+        App.Bubblesort();
     }
 }
